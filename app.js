7bae266abd5fea0fe5f26514fbd04748713dfafd
heimlichit <90272694+heimlichit@users.noreply.github.com>
2022-12-04 14:53:06 -0500

    Layout, get, push, onclick function

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var cors = require("cors");
+/** @format */
+
+var express = require('express');
+var mongoose = require('mongoose');
+var cors = require('cors');
 var app = express();
-var database = require("./config/database");
-var bodyParser = require("body-parser");
-var path = require("path");
+var database = require('./config/database');
+var bodyParser = require('body-parser');
+var path = require('path');
 // pull information from HTML POST (express4)
 
-const hbs = require("express-handlebars");
-app.use(express.static(path.join(__dirname, "public")));
-app.engine(".hbs", hbs.engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
+const hbs = require('express-handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 var port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 // var db = require("./db");
 // db.initialize();
 mongoose.connect(database.url);
 
-var Restaurant = require("./models/Restaurant");
+var { Restaurant } = require('./models/Restaurant');
+var { ReviewModel } = require('./models/Restaurant');
 
 //get all restaurant data from db
-app.get("/api/restaurants", async function (req, res) {
+app.get('/api/restaurants', async function (req, res) {
 	//req.query to parsed query string parameteres
 	//req.params to parsed route parameters from path
 	let page = req.query.page;
@@ -42,8 +45,8 @@ app.get("/api/restaurants", async function (req, res) {
 
 	Restaurant.find(function (err, restaurants) {
 		if (err) res.send(err);
-		res.render("restaurant", {
-			layout: "main.hbs",
+		res.render('restaurant', {
+			layout: 'main.hbs',
 			data: restaurants,
 		});
 	})
@@ -52,7 +55,7 @@ app.get("/api/restaurants", async function (req, res) {
 		.lean();
 });
 
-app.post("api/restaurants", function (req, res) {
+app.post('api/restaurants', async function (req, res) {
 	console.log(req.body);
 	Restaurant.create(
 		{
@@ -76,8 +79,8 @@ app.post("api/restaurants", function (req, res) {
 			}
 			Restaurant.find(function (err, restaurants) {
 				if (err) res.send(err);
-				res.render("restaurant", {
-					layout: "main.hbs",
+				res.render('restaurant', {
+					layout: 'main.hbs',
 					data: restaurants,
 				});
 			})
@@ -89,32 +92,32 @@ app.post("api/restaurants", function (req, res) {
 });
 
 //put  restaurant based on the _id number
-app.put('/api/restaurants/:restaurantId', function(req, res) {
+app.put('/api/restaurants/:restaurantId', function (req, res) {
 	// create mongose method to update an existing record into collection
 	let id = req.params.restaurantId;
 	var data = {
 		address: [req.body.address],
-			building: req.body.building,
-			coord: [req.body.coord],
-			street: req.body.street,
-			zipcode: req.body.zipcode,
-			borough: req.body.borough,
-			cuisine: req.body.cuisine,
-			grades: [req.body.grades],
-			name: req.body.name,
-			restaurant_id: req.body.restaurant_id
-	}
+		building: req.body.building,
+		coord: [req.body.coord],
+		street: req.body.street,
+		zipcode: req.body.zipcode,
+		borough: req.body.borough,
+		cuisine: req.body.cuisine,
+		grades: [req.body.grades],
+		name: req.body.name,
+		restaurant_id: req.body.restaurant_id,
+	};
 
 	// save the user
-	Restaurant.findByIdAndUpdate(id, data, function(err, restaurant) {
-	if (err) throw err;
+	Restaurant.findByIdAndUpdate(id, data, function (err, restaurant) {
+		if (err) throw err;
 
-	res.send('Successfully updated restaurants - '+restaurant.name);
+		res.send('Successfully updated restaurants - ' + restaurant.name);
 	});
 });
 
 // delete restaurant based on _id number
-app.delete("/api/restaurants/:restaurantId", function (req, res) {
+app.delete('/api/restaurants/:restaurantId', function (req, res) {
 	console.log(req.params.restaurantId);
 	let id = req.params.restaurantId;
 	restaurant.remove(
@@ -123,10 +126,10 @@ app.delete("/api/restaurants/:restaurantId", function (req, res) {
 		},
 		function (err) {
 			if (err) res.send(err);
-			else res.send("Restaurant with" + _id + "has been Deleted.");
+			else res.send('Restaurant with' + _id + 'has been Deleted.');
 		}
 	);
 });
 
 app.listen(port);
-console.log("App listening on port : " + port);
+console.log('App listening on port : ' + port);
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,28 +1,30 @@
+/** @format */
+
 // load mongoose since we need it to define a model
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-ReviewSchema = new Schema({
-  date: Date,
-  grade: String,
-  score: Number,
+var ReviewSchema = new Schema({
+	date: Date,
+	grade: String,
+	score: Number,
 });
 
-RestaurantSchema = new Schema({
-  address: {
-    building: String,
-    coord: [Number],
-    street: String,
-    zipcode: String,
-  },
-  borough: String,
-  cuisine: String,
-  grades: [ReviewSchema],
-  name: String,
-  restaurant_id: String,
+var RestaurantSchema = new Schema({
+	address: {
+		building: String,
+		coord: [Number],
+		street: String,
+		zipcode: String,
+	},
+	borough: String,
+	cuisine: String,
+	grades: [ReviewSchema],
+	name: String,
+	restaurant_id: String,
 });
 
-var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
-var ReviewModel = mongoose.model("Review", ReviewSchema);
+var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+var ReviewModel = mongoose.model('Review', ReviewSchema);
 
-module.exports = { RestaurantModel, ReviewModel };
+module.exports = { Restaurant, ReviewModel };
